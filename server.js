dcec6e2a3b45472a76b6c0c6fd15202a9ab48cf6
cdplourde <cdplourde@gmail.com>
2018-06-08 03:58:51 -0700

    server and handlebars initial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+app.set("port", (process.env.PORT || 8080));
+
+app.use("/public", express.static("public"));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+require("./controllers/routes")(app);
+
+app.listen(app.get("port"), function() {
+  console.log("App listening on PORT " + app.get("port"));
+});``
